557c8d11bce68085acab592c3d1c57918bf278cf
Rafliia <raflialbn@gmail.com>
2021-02-23 19:42:55 -0800

    covidapp

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,35 +1,174 @@
-import 'package:fl_chart/fl_chart.dart';
+import 'package:pandemic/constants.dart';
+import 'package:pandemic/screen/details_screen.dart';
+import 'package:pandemic/widgets/info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:pandemic/constants.dart';
-import 'package:pandemic/widgets/line_chart.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: kPrimaryColor.withOpacity(0.03),
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(50),
-            bottomRight: Radius.circular(50),
-          ),
+
+//wrap singlechildscrollview for correct displaying in small density devices
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding:
+                  EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: kPrimaryColor.withOpacity(0.03),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: <Widget>[
+                  InfoCard(
+                    title: "Confirmed Cases",
+                    iconColor: Color(0xFFFF8C00),
+                    effectedNum: 1062,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Deaths",
+                    iconColor: Color(0xFFFF2D55),
+                    effectedNum: 75,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Recovered",
+                    iconColor: Color(0xFF50E3C2),
+                    effectedNum: 689,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "New Cases",
+                    iconColor: Color(0xFF5856D6),
+                    effectedNum: 75,
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DetailsScreen();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Preventions",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    buildPreventation(),
+                    SizedBox(height: 40),
+                    buildHelpCard(context)
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
-        child: Wrap(runSpacing: 20, children: <Widget>[
-          infoCard(
-            title: "Confirmed Cases",
-            iconColor: Color(0xFFFF8C00),
-            effectedNum: 1062,
+      ),
+    );
+  }
+
+  Row buildPreventation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        PreventitonCard(
+          svgSrc: "assets/icons/hand_wash.svg",
+          title: "Wash Hands",
+        ),
+        PreventitonCard(
+          svgSrc: "assets/icons/use_mask.svg",
+          title: "Use Masks",
+        ),
+        PreventitonCard(
+          svgSrc: "assets/icons/Clean_Disinfect.svg",
+          title: "Clean Disinfect",
+        ),
+      ],
+    );
+  }
+
+  Container buildHelpCard(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              // left side padding is 40% of total width
+              left: MediaQuery.of(context).size.width * .4,
+              top: 20,
+              right: 20,
+            ),
+            height: 130,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF60BE93),
+                  Color(0xFF1B8D59),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Dial 999 for \nMedical Help!\n",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.white),
+                  ),
+                  TextSpan(
+                    text: "If any symptoms appear",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          infoCard(
-            title: "Confirmed Cases",
-            iconColor: Color(0xFFFF8C00),
-            effectedNum: 1062,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: SvgPicture.asset("assets/icons/nurse.svg"),
           ),
-        ]),
+          Positioned(
+            top: 30,
+            right: 10,
+            child: SvgPicture.asset("assets/icons/virus.svg"),
+          ),
+        ],
       ),
     );
   }
@@ -52,93 +191,28 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class infoCard extends StatelessWidget {
+class PreventitonCard extends StatelessWidget {
+  final String svgSrc;
   final String title;
-  final int effectedNum;
-  final Color iconColor;
-
-  const infoCard({
+  const PreventitonCard({
     Key key,
+    this.svgSrc,
     this.title,
-    this.effectedNum,
-    this.iconColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Container(
-          width: constraints.maxWidth / 2 - 10,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.center,
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: iconColor.withOpacity(0.12),
-                        shape: BoxShape.circle,
-                      ),
-                      child: SvgPicture.asset(
-                        "assets/icons/running.svg",
-                        height: 12,
-                        width: 12,
-                      ),
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: RichText(
-                        text: TextSpan(
-                          style: TextStyle(color: kTextColor),
-                          children: [
-                            TextSpan(
-                                text: "$effectedNum \n",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .title
-                                    .copyWith(fontWeight: FontWeight.bold)),
-                            TextSpan(
-                                text: "People",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  height: 2,
-                                ))
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: LineReportChart(),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+    return Column(
+      children: <Widget>[
+        SvgPicture.asset(svgSrc),
+        Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .bodyText1
+              .copyWith(color: kPrimaryColor),
+        )
+      ],
     );
   }
 }
--- lib/widgets/line_chart.dart
+++ lib/widgets/line_chart.dart
@@ -1,6 +1,6 @@
+import 'package:pandemic/constants.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:pandemic/constants.dart';
 
 class LineReportChart extends StatelessWidget {
   @override
@@ -34,10 +34,10 @@ class LineReportChart extends StatelessWidget {
       FlSpot(2, .5),
       FlSpot(3, .7),
       FlSpot(4, .2),
-      FlSpot(5, .2),
+      FlSpot(5, 2),
       FlSpot(6, 1.5),
       FlSpot(7, 1.7),
-      FlSpot(8, .1),
+      FlSpot(8, 1),
       FlSpot(9, 2.8),
       FlSpot(10, 2.5),
       FlSpot(11, 2.65),
